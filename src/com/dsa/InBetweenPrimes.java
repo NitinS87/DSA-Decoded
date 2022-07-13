5aaf237fca1489c34967112bb647e49d5d299c59
Nitin <nitins9868@gmail.com>
2022-07-13 18:15:23 +0530

    First Commit

---
+++ src/com/dsa/InBetweenPrimes.java
@@ -0,0 +1,26 @@
+package com.dsa;
+
+import java.util.Scanner;
+
+public class InBetweenPrimes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextByte();
+        int high = sc.nextInt();
+        for(int i=low;i<=high;i++){
+            boolean flag = false;
+            for(int div=2;div<=high/2;div++){
+                if(i%div==0){
+                    flag = true;
+                    break;
+                }
+                else
+                    flag = false;
+            }
+            if(!flag){
+                System.out.print(i + ", ");
+            }
+        }
+
+    }
+}
