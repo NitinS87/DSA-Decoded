97a5701cbdff8c66c05926213ef913ac3b18b672
Nitin <nitins9868@gmail.com>
2022-07-19 13:42:47 +0530

    New Array Class added

---
+++ src/com/array/ArrayClass.java
@@ -0,0 +1,72 @@
+package com.array;
+
+public class ArrayClass {
+    private int[] items;
+    private int count;
+
+    public ArrayClass(int length){
+        items = new int[length];
+    }
+
+    public void print(){
+        for(int i=0;i<count;i++){
+            System.out.println(items[i]);
+        }
+    }
+
+    public void insert(int item){
+        //if array is full, resize it
+        if(items.length == count){
+            //create new array with double length
+            int[] newItems = new int[2* count];
+
+            //copy all existing items
+            for(int i=0;i<count;i++){
+                newItems[i] = items[i];
+            }
+
+            //set new array as old array
+            items = newItems;
+
+        }
+
+        //add the new item at the end
+        items[count++] = item;
+    }
+
+    public void removeAt(int index){
+        //validate the index
+        if(index<0 || index>=count)
+            throw new IllegalArgumentException();
+
+        //shift the items to the left to fill hole
+        for(int i = index; i<count;i++){
+            items[i] = items[i+1];
+        }
+
+        count--;
+    }
+
+    public int indexOf(int item){
+        //traverse through the array
+        for(int i=0;i<count;i++){
+            if(item == items[i]) {
+                return i;
+            }
+        }
+
+        //if not found return -1
+        return -1;
+    }
+
+    public int max(){
+        int maxItem = items[0];
+        for(int i=0;i< count;i++){
+            if(maxItem< items[i])
+                maxItem = items[i];
+        }
+
+        return maxItem;
+    }
+
+}
---
+++ src/com/array/ArrayMain.java
@@ -0,0 +1,17 @@
+package com.array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        ArrayClass arr = new ArrayClass(5);
+        arr.insert(5);
+        arr.insert(10);
+        arr.insert(15);
+        arr.insert(20);
+        arr.insert(25);
+//        arr.removeAt(5);
+        System.out.println(arr.indexOf(25));
+        System.out.println(arr.max());
+        arr.print();
+
+    }
+}
